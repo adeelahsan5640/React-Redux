176c6f8bb7e225968adf83d6dcfe055e432669ab
Adeel Ahsan <58143352+adeelahsan5640@users.noreply.github.com>
2021-04-16 02:29:59 +0500

    completed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
-import { createStore } from 'redux'
-import cakeReducer from './cakes/cakeReducer'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import logger from 'redux-logger'
+import rootReducer from './rootReducer'
 
-const store = createStore(cakeReducer)
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)))
 export default store
